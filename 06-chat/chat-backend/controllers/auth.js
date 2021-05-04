0e23b2becb7f396b676f021439c3d3f9842fdba9
Javier Ramos Nodal <javiramos@soliss.com>
2021-05-04 22:11:00 +0200

    111

--- 06-chat/chat-backend/controllers/auth.js
+++ 06-chat/chat-backend/controllers/auth.js
@@ -1,23 +1,41 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
+const Usuario = require('../models/usuario');
 
 const crearUsuario = async (req, res = response) => {
-    res.json({
-        ok: true,
-        usuario: 'ABC'
-    })
+    try {
+        const { email, password } = req.body;
+
+        // verificar que el email no exista
+        const existeEmail = await Usuario.findOne({ email });
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya existe'
+            });
+        }
+
+        const usuario = new Usuario(req.body);
+        
+        // encriptar contraseña
+        
+
+        // guardar usuario en BD
+        await usuario.save();
+
+        res.json({
+            usuario
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
 }
 
 const login = async (req, res = response) => {
-
-    const errores = validationResult(req);
-
-    if (!errores.isEmpty()) {
-        return res.status(400).json({
-            ok: false,
-            errors: errores.mapped()
-        });
-    }
 
     const { email, password } = req.body;
 
--- 06-chat/chat-backend/router/auth.js
+++ 06-chat/chat-backend/router/auth.js
@@ -5,15 +5,22 @@
 const { Router } = require('express');
 const { crearUsuario, login, renewToken } = require('../controllers/auth');
 const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
-router.post('/new', crearUsuario)
+router.post('/new', [
+    check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    validarCampos
+], crearUsuario)
 
 // Login
 router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
-    check('password', 'El password es obligatorio').not().isEmpty()
+    check('password', 'El password es obligatorio').not().isEmpty(),
+    validarCampos
 ], login)
 
 // Revalidar token
